a40384d6fe629b24d130ab85581c1a379f38b16e
Kirubel Eshetu <Kirubelwinner@gmail.com>
2025-03-08 09:40:53 +0300

    Update emailotp.js

--- emailotp.js
+++ emailotp.js
@@ -6,30 +6,30 @@ function sendOTP() {
 
 	let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
 	Email.send({
-    SecureToken : "fbebd213-4ddf-45d9-b205-3f5313s01cs3",
-    To : email.value,
-    From : "Legacyadmin",
-    Subject : "Email OTP using JavaScript",
-    Body : emailbody,
-}).then(
+		SecureToken: "fbebd213-4ddf-45d9-b205-3f5313s01cs3",
+		To: email.value,
+		From: "Legacyadmin",
+		Subject: "Email OTP using JavaScript",
+		Body: emailbody,
+	}).then(
 
-	message => {
-		if (message === "OK") {
-			alert("OTP sent to your email " + email.value);
+		message => {
+			if (message === "OK") {
+				alert("OTP sent to your email " + email.value);
 
-			otpverify.style.display = "flex";
-			const otp_inp = document.getElementById('otp_inp');
-			const otp_btn = document.getElementById('otp-btn');
+				otpverify.style.display = "flex";
+				const otp_inp = document.getElementById('otp_inp');
+				const otp_btn = document.getElementById('otp-btn');
 
-			otp_btn.addEventListener('click', () => {
-				if (otp_inp.value == otp_val) {
-					alert("Email address verified...");
-				}
-				else {
-					alert("Invalid OTP");
-				}
-			})
+				otp_btn.addEventListener('click', () => {
+					if (otp_inp.value == otp_val) {
+						alert("Email address verified...");
+					}
+					else {
+						alert("Invalid OTP");
+					}
+				})
+			}
 		}
-	}
-);
+	);
 }
